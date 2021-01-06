bd477b3e607921d86a38190e4aab8871dbd543b1
Вигдорова Алина <themetanka@gmail.com>
2021-01-06 12:06:03 +0300

    Добавление пробелов

--- src/app/components/top-menu/TopMenu.tsx
+++ src/app/components/top-menu/TopMenu.tsx
@@ -49,16 +49,16 @@ const useStyles = makeStyles((theme: Theme) =>
             borderRadius: theme.shape.borderRadius,
             backgroundColor: fade(theme.palette.common.white, 0.15),
             '&:hover': {
-              backgroundColor: fade(theme.palette.common.white, 0.25),
+                backgroundColor: fade(theme.palette.common.white, 0.25),
             },
             marginRight: theme.spacing(2),
             marginLeft: 0,
             width: '100%',
             [theme.breakpoints.up('sm')]: {
-              marginLeft: theme.spacing(3),
-              width: 'auto',
+                marginLeft: theme.spacing(3),
+                width: 'auto',
             },
-          },
+        },
     })
 );
 
