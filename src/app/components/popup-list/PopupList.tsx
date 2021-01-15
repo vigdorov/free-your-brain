b1d19bea22f7a14e91d135d82cb35698741b1532
Maxx Nikitin <maxxnikitin90@yandex.ru>
2021-01-15 17:41:44 +0300

    Добавить открытие\закрытие модалки создания задачи #63
    написал каркас функции и рендеринга

--- src/app/components/popup-list/PopupList.tsx
+++ src/app/components/popup-list/PopupList.tsx
@@ -1,13 +1,16 @@
+import {useHistory} from 'react-router-dom';
 import {Dialog, List, ListItem, ListItemText} from '@material-ui/core';
 import React, {Fragment, memo, PropsWithChildren, useCallback} from 'react';
 import {v4} from 'uuid';
 import {MENU_ADDS} from '../../consts';
+import {PageType} from '../../../core/enums/common';
+import {buildPath} from '../../../core/utils/buildPath';
 
-type Props = PropsWithChildren<{
-}>;
+type Props = PropsWithChildren<{}>;
 
 const PopupList: React.FC<Props> = ({children}) => {
     const [open, setOpen] = React.useState(false);
+    const history = useHistory();
 
     const handleClickOpen = useCallback(() => {
         setOpen(true);
@@ -17,20 +20,29 @@ const PopupList: React.FC<Props> = ({children}) => {
         setOpen(false);
     }, [setOpen]);
 
+    const handleAddTicket = useCallback(() => {
+        // History.push(buildPath({pageType: PageType.Main, query[modal]: ModalType.CreateTask}));
+        setOpen(false);
+    }, [history]);
+
     return (
         <Fragment>
             <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
                 <List>
-                    {MENU_ADDS.map(item => (
-                        <ListItem button onClick={handleClose} key={v4()}>
-                            <ListItemText primary={item} />
-                        </ListItem>
-                    ))}
+                    {MENU_ADDS.map(item =>
+                        item === 'Добавить задачу' ? (
+                            <ListItem button onClick={handleAddTicket} key={v4()}>
+                                <ListItemText primary={item} />
+                            </ListItem>
+                        ) : (
+                            <ListItem button onClick={handleClose} key={v4()}>
+                                <ListItemText primary={item} />
+                            </ListItem>
+                        )
+                    )}
                 </List>
             </Dialog>
-            <div onClick={handleClickOpen}>
-                {children}
-            </div>
+            <div onClick={handleClickOpen}>{children}</div>
         </Fragment>
     );
 };
