38d70b3f39109ed92592390fa4102dfd1ff0cb98
Max Nikitin <61289736+Maxxnikitin@users.noreply.github.com>
2021-02-04 11:49:28 +0300

    Изменение способа открытия модалки выбора создания (#80)
    Изменение способа открытия модалки выбора создания

--- src/app/components/popup-list/PopupList.tsx
+++ src/app/components/popup-list/PopupList.tsx
@@ -1,27 +1,35 @@
 import {Dialog, List} from '@material-ui/core';
-import React, {Fragment, memo, PropsWithChildren, useCallback} from 'react';
+import React, {Fragment, memo, PropsWithChildren, useCallback, useMemo} from 'react';
+import {useHistory} from 'react-router-dom';
+import {PageType} from '_enums/common';
+import {buildPath} from '_utils/buildPath';
+import {useQuery} from '_hooks/useQuery';
+import {ModalType} from '../../../app/enums';
 import {MENU_ADDS} from '../../consts';
 import PopupListItem from '../popup-list-item';
+import {queryParsers} from '../../../app/utils';
 
 type Props = PropsWithChildren<{}>;
 
 const PopupList: React.FC<Props> = ({children}) => {
-    const [open, setOpen] = React.useState(false);
+    const {modal} = useQuery(queryParsers);
+    const isPopupListOpen = useMemo(() => modal === ModalType.CreateModal, [modal]);
+    const history = useHistory();
 
     const handleClickOpen = useCallback(() => {
-        setOpen(true);
-    }, [setOpen]);
+        history.push(buildPath({pageType: PageType.Main, query: {modal: ModalType.CreateModal}}));
+    }, [history]);
 
     const handleClose = useCallback(() => {
-        setOpen(false);
-    }, [setOpen]);
+        history.push(buildPath({pageType: PageType.Main}));
+    }, [history]);
 
     return (
         <Fragment>
-            <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+            <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={isPopupListOpen}>
                 <List>
                     {MENU_ADDS.map(item => (
-                        <PopupListItem item={item.text} url={item.url} setOpen={setOpen} key={item.id} />
+                        <PopupListItem item={item.text} url={item.url} key={item.id} />
                     ))}
                 </List>
             </Dialog>
